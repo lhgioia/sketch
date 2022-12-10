9680f5dfefa2020445739d4005ecbd277c0a481a
lhgioia <lhgioia@gmail.com>
2022-12-10 11:04:23 -0800

    Add basic js backbone

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // create 16x16 div grid using flexbox attributes
+
+const gridSideLength = 16;
+
+const wrapperDiv = document.querySelector('.wrapperDiv');
+
+const pixelDiv = document.createElement('div');
+pixelDiv.classList.add('new');
+
+wrapperDiv.appendChild(pixelDiv);
