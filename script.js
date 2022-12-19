f1416b3fafcf8793e78c58dcaeb673ce4d650de8
lhgioia <lhgioia@gmail.com>
2022-12-18 17:36:17 -0800

    Add building blocks to script.js
    Doing all css with inline javascript for practice. Turned off the
    style.css link in index.html

--- script.js
+++ script.js
@@ -2,9 +2,18 @@
 
 const gridSideLength = 16;
 
-const wrapperDiv = document.querySelector('.wrapperDiv');
+const gridPixels = 800;
+
+const wrapperDiv = document.querySelector('#wrapperDiv');
+
+wrapperDiv.style.display = 'grid';
+
+// want to get more familiar with css grid https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout/Basic_Concepts_of_Grid_Layout
+
 
 const pixelDiv = document.createElement('div');
 pixelDiv.classList.add('new');
 
 wrapperDiv.appendChild(pixelDiv);
+
+pixelDiv.setAttribute('style', 'color: blue; background: white;');
