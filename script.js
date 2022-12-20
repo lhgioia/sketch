af76e31de12b8f19e11ed33f43b7a4a23bcaa929
lhgioia <lhgioia@gmail.com>
2022-12-20 11:32:39 -0800

    Add button and make everything with js
    Button is very small for some reason. Also deleted the style sheet and
    the starter div. Only using body to start now.

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 // create sketch grid using css grid
 
-const gridSideLength = 160;
+const gridSideLength = 100;
 
 const gridDivNumber = gridSideLength * gridSideLength;
 
 const gridPixels = 750;
 
-const wrapperDiv = document.querySelector('#wrapperDiv');
+const body = document.querySelector('body');
+
+const buttonDiv = document.createElement('div');
+body.appendChild(buttonDiv);
+
+const button = document.createElement('button');
+buttonDiv.appendChild(button);
+
+const wrapperDiv = document.createElement('div');
+body.appendChild(wrapperDiv);
+// const wrapperDiv = document.querySelector('#wrapperDiv');
 
 wrapperDiv.style.height = `${gridPixels}px`;
 wrapperDiv.style.width = `${gridPixels}px`;
@@ -23,4 +33,4 @@ for (let i = 0; i < gridDivNumber; i++) {
 
 function changeBackground(e) {
     e.target.style.background = 'black';
-}
+};
